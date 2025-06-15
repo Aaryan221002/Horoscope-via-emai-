1a8aa467c2e2ffce57e8bbc5a6abcf6dc0935355
Aaryan Sachdeva <sachdeva.aaryan22@gmail.com>
2025-06-15 22:51:30 +0530

    ABC

--- horoscope.py
+++ horoscope.py
@@ -207,7 +207,5 @@ emailentry.grid(row=7, column=10)
 #Button & packing it and assigning it a command
 Button(text="Submit", command=printvals).grid(row=10, column=10)
 root.mainloop()
-#A change done in branch
-#github ui change
-#another change
+
 
